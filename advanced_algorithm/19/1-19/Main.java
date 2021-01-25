5382a2e393384eabda08cd89ebabc6d765019f78
zlic <MF20320220@smail.nju.edu.cn>
2021-01-25 23:04:35 +0800

    ✨feat(高级算法): 添加题库代码模板和 README 文件。

---
+++ advanced_algorithm/19/1-19/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    private Scanner scanner;
+    
+    public static void main(String[] args) {
+        new Main().exec();
+    }
+    
+    private void exec() {
+        this.scanner = new Scanner(System.in);
+        this.certainExec();
+        this.scanner.close();
+    }
+    
+    private void certainExec() {
+        // TODO
+    }
+}
