c331c0133f6c77628b89555120b5ed7d86d6ccad
zlic <MF20320220@smail.nju.edu.cn>
2021-01-26 14:02:47 +0800

    ✨feat(高级算法): 模板代码添加方法模板。

--- advanced_algorithm/template/Main.java
+++ advanced_algorithm/template/Main.java
@@ -14,6 +14,39 @@ public class Main {
     }
     
     private void certainExec() {
+        String caseCountStr = this.scanner.nextLine();
+        int caseCount = Integer.parseInt(caseCountStr);
+        for (int i = 0; i < caseCount; i++) {
+            this.dealProblem();
+        }
+    }
+    
+    public String toString() {
+        // TODO 修改属性信息
+        StringBuilder builder = new StringBuilder();
+        return builder.toString();
+    }
+    
+    void printData() {
+        System.out.println(this);
+    }
+
+    // TODO 修改方法名称
+    private void dealProblem() {
+        loadData();
+        calResult();
+        printResult();
+    }
+
+    private void loadData() {
+        // TODO
+    }
+
+    private void calResult() {
+        // TODO
+    }
+
+    private void printResult() {
         // TODO
     }
 }
