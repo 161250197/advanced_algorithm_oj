e080668b4f405fac5840d939e0543ad3e9b15146
zlic <MF20320220@smail.nju.edu.cn>
2021-01-28 17:32:23 +0800

    ✨feat(高级算法): 添加工具类文件。

---
+++ advanced_algorithm/template/Util.java
@@ -0,0 +1,23 @@
+public class Util {
+    private String intArrToString(int[] ints) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[ " + ints[0]);
+        int arrLen = ints.length;
+        for (int i = 1; i < arrLen; i++) {
+            builder.append("," + ints[i]);
+        }
+        builder.append(" ];");
+        return builder.toString();
+    }
+    
+    private String longArrToString(long[] longs) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[ " + longs[0]);
+        long arrLen = longs.length;
+        for (long i = 1; i < arrLen; i++) {
+            builder.append("," + longs[i]);
+        }
+        builder.append(" ];");
+        return builder.toString();
+    }
+}
