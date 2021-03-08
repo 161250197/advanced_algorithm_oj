a2675b3cfa1b4f51a3d6b6200b8cb706471821b6
zlic <MF20320220@smail.nju.edu.cn>
2021-03-08 19:21:44 +0800

    ✨feat(高级算法): 2-7 Searching_3。

--- advanced_algorithm/21/2-7/Main.java
+++ advanced_algorithm/21/2-7/Main.java
@@ -17,15 +17,39 @@ public class Main {
         String caseCountStr = this.scanner.nextLine();
         int caseCount = Integer.parseInt(caseCountStr);
         for (int i = 0; i < caseCount; i++) {
-            this.dealProblem();
+            this.tellMark();
+        }
+    }
+    
+    class ArrToString<T> {
+        String exec(T[] arr) {
+            if (arr == null) {
+                return "null";
+            }
+            int arrLen = arr.length;
+            if (arrLen == 0) {
+                return "[]";
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.append("[ " + arr[0]);
+            for (int i = 1; i < arrLen; i++) {
+                builder.append("," + arr[i]);
+            }
+            builder.append(" ]");
+            return builder.toString();
         }
     }
     
     public String toString() {
-        // TODO 修改属性信息
+        ArrToString<Long> longArrToString = new ArrToString<>();
+
         StringBuilder builder = new StringBuilder();
         builder
-            .append("test: " + this.test + ";\n")
+            .append("n: " + this.n + ";\n")
+            .append("q: " + this.q + ";\n")
+            .append("startArr: " + longArrToString.exec(this.startArr) + ";\n")
+            .append("countArr: " + longArrToString.exec(this.countArr) + ";\n")
+            .append("qArr: " + longArrToString.exec(this.qArr) + ";\n")
         ;
         return builder.toString();
     }
@@ -34,26 +58,82 @@ public class Main {
         System.out.println(this);
     }
 
-    // TODO 添加属性
-    String test = "test";
+    int n = -1;
+    int q = -1;
+    Long[] startArr = null;
+    Long[] countArr = null;
+    Long[] qArr = null;
 
-    // TODO 修改方法名称
-    private void dealProblem() {
+    private void tellMark() {
         loadData();
         calResult();
         printResult();
     }
 
     private void loadData() {
-        // TODO
+        this.loadNAndQ();
+        this.loadGroups();
+        this.loadQArr();
+    }
+
+    private void loadNAndQ() {
+        String[] nAndQStrArr = this.scanner.nextLine().split(" ");
+        this.n = Integer.parseInt(nAndQStrArr[0]);
+        this.q = Integer.parseInt(nAndQStrArr[1]);
+    }
+
+    private void loadGroups() {
+        this.startArr = new Long[this.n];
+        this.countArr = new Long[this.n];
+        String[] scoreStrArr = this.scanner.nextLine().split(" ");
+        for (int i = 0; i < this.n; i++) {
+            int startI = i * 2;
+            int lastI = startI + 1;
+            this.startArr[i] = Long.parseLong(scoreStrArr[startI]);
+            this.countArr[i] = Long.parseLong(scoreStrArr[lastI]) - this.startArr[i] + 1;
+        }
+    }
+    
+    private void loadQArr() {
+        this.qArr = new Long[this.q];
+        String[] rankStrArr = this.scanner.nextLine().split(" ");
+        for (int i = 0; i < this.q; i++) {
+            this.qArr[i] = Long.parseLong(rankStrArr[i]);
+        }
     }
 
     private void calResult() {
-        // TODO
-        this.printData();
+        long firstQ = this.qArr[0];
+        long score = this.calScore(firstQ);
+        this.printFirstScore(score);
+        for (int i = 1; i < this.q; i++) {
+            long q = this.qArr[i];
+            score = this.calScore(q);
+            this.printScore(score);
+        } 
+    }
+
+    private long calScore(long q) {
+        for (int i = 0; i < this.n; i++) {
+            long count = this.countArr[i];
+            if (q <= count) {
+                return this.startArr[i] + q - 1;
+            }
+            q = q - count; 
+        }
+        return -1;
+    }
+
+    private void printFirstScore(long score) {
+        System.out.print(score);
+    }
+
+    private void printScore(long score) {
+        System.out.print(" ");
+        System.out.print(score);
     }
 
     private void printResult() {
-        // TODO
+        System.out.println();
     }
 }
