2361091911294aa5e65063d3d390cafa56e3897a
zlic <MF20320220@smail.nju.edu.cn>
2021-03-08 15:32:09 +0800

    ✨feat(高级算法): 2-3 深度优先遍历。

--- advanced_algorithm/21/2-3/Main.java
+++ advanced_algorithm/21/2-3/Main.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
     private Scanner scanner;
@@ -17,15 +23,30 @@ public class Main {
         String caseCountStr = this.scanner.nextLine();
         int caseCount = Integer.parseInt(caseCountStr);
         for (int i = 0; i < caseCount; i++) {
-            this.dealProblem();
+            this.depthFirstSearch();
         }
+    }
+
+    private String stringArrToString(String[] strs) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[ " + strs[0]);
+        int arrLen = strs.length;
+        for (int i = 1; i < arrLen; i++) {
+            builder.append("," + strs[i]);
+        }
+        builder.append(" ]");
+        return builder.toString();
     }
     
     public String toString() {
-        // TODO 修改属性信息
         StringBuilder builder = new StringBuilder();
         builder
-            .append("test: " + this.test + ";\n")
+            .append("vertexCount: " + this.vertexCount + ";\n")
+            .append("firstVertex: " + this.firstVertex + ";\n")
+            .append("vertexArr: " + this.stringArrToString(this.vertexArr) + ";\n")
+            .append("vertexToAdjacentVertexListMap: " + this.vertexToAdjacentVertexListMap.toString() + ";\n")
+            .append("searchedVertexSet: " + this.searchedVertexSet + ";\n")
+            .append("maxDepth: " + this.maxDepth + ";\n")
         ;
         return builder.toString();
     }
@@ -34,26 +55,97 @@ public class Main {
         System.out.println(this);
     }
 
-    // TODO 添加属性
-    String test = "test";
+    int vertexCount = -1;
+    String firstVertex = null;
+    String[] vertexArr = null;
+    Map<String, List<String>> vertexToAdjacentVertexListMap = null;
+    Set<String> searchedVertexSet = null;
+    int maxDepth = -1;
 
-    // TODO 修改方法名称
-    private void dealProblem() {
+    private void depthFirstSearch() {
         loadData();
         calResult();
         printResult();
     }
 
     private void loadData() {
-        // TODO
+        this.searchedVertexSet = new HashSet<>();
+        this.maxDepth = -1;
+
+        this.loadVertexCountAndFirstVertex();
+        this.loadVertexArr();
+        this.loadAdjacentMatrix();
+    }
+
+    private void loadVertexCountAndFirstVertex() {
+        String[] vertexCountAndFirstVertexStrArr = this.scanner.nextLine().split(" ");
+        this.vertexCount = Integer.parseInt(vertexCountAndFirstVertexStrArr[0]);
+        this.firstVertex = vertexCountAndFirstVertexStrArr[1];
+    }
+
+    private void loadVertexArr() {
+        this.vertexArr = this.scanner.nextLine().split(" ");
+    }
+
+    private void loadAdjacentMatrix() {
+        this.vertexToAdjacentVertexListMap = new HashMap<>();
+        for (int i = 0; i < this.vertexCount; i++) {
+            this.loadAdjacentMatrixRow();
+        }
+    }
+
+    private void loadAdjacentMatrixRow() {
+        String[] vertexAndAdjacentVertexArr = this.scanner.nextLine().split(" ");
+        String vertex = vertexAndAdjacentVertexArr[0];
+        List<String> adjacentVertexList = new ArrayList<>();
+        for (int i = 0; i < this.vertexCount; i++) {
+            if (vertexAndAdjacentVertexArr[i + 1].equals("1")) {
+                adjacentVertexList.add(this.vertexArr[i]);
+            }
+        }
+        this.vertexToAdjacentVertexListMap.put(vertex, adjacentVertexList);
     }
 
     private void calResult() {
-        // TODO
-        this.printData();
+        this.findAdjacentVertex(this.firstVertex, 0);
+    }
+
+    private void findAdjacentVertex(String vertex, int depth) {
+        this.setSearched(vertex);
+        depth = depth + 1;
+
+        List<String> adjacentVertexList = this.vertexToAdjacentVertexListMap.get(vertex);
+        boolean end = true;
+        for (String adjacentVertex : adjacentVertexList) {
+            if (!this.searched(adjacentVertex)) {
+                end = false;
+                this.findAdjacentVertex(adjacentVertex, depth);
+            }
+        }
+
+        if (end) {
+            this.updateMaxDepth(depth);
+        }
+        this.resetSearched(vertex);
+    }
+
+    private boolean searched(String vertex) {
+        return this.searchedVertexSet.contains(vertex);
+    }
+
+    private void setSearched(String vertex) {
+        this.searchedVertexSet.add(vertex);
+    }
+
+    private void resetSearched(String vertex) {
+        this.searchedVertexSet.remove(vertex);
+    }
+
+    private void updateMaxDepth(int depth) {
+        this.maxDepth = Math.max(depth, this.maxDepth);
     }
 
     private void printResult() {
-        // TODO
+        System.out.println(this.maxDepth);
     }
 }
