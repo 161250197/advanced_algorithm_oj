b01f3311e1a0f2cae591465d48da41f6ea017c18
zlic <MF20320220@smail.nju.edu.cn>
2021-03-08 15:51:21 +0800

    ✨feat(高级算法): 工具类重构。
    删除无用方法；
    添加输入数组为空数组的处理方法。

--- advanced_algorithm/template/Util.java
+++ advanced_algorithm/template/Util.java
@@ -1,8 +1,11 @@
 public class Util {
     private String intArrToString(int[] ints) {
+        int arrLen = ints.length;
+        if (arrLen == 0) {
+            return "[]";
+        }
         StringBuilder builder = new StringBuilder();
         builder.append("[ " + ints[0]);
-        int arrLen = ints.length;
         for (int i = 1; i < arrLen; i++) {
             builder.append("," + ints[i]);
         }
@@ -11,9 +14,12 @@ public class Util {
     }
     
     private String longArrToString(long[] longs) {
+        int arrLen = longs.length;
+        if (arrLen == 0) {
+            return "[]";
+        }
         StringBuilder builder = new StringBuilder();
         builder.append("[ " + longs[0]);
-        int arrLen = longs.length;
         for (int i = 1; i < arrLen; i++) {
             builder.append("," + longs[i]);
         }
@@ -22,23 +28,16 @@ public class Util {
     }
 
     private String stringArrToString(String[] strs) {
+        int arrLen = strs.length;
+        if (arrLen == 0) {
+            return "[]";
+        }
         StringBuilder builder = new StringBuilder();
         builder.append("[ " + strs[0]);
-        int arrLen = strs.length;
         for (int i = 1; i < arrLen; i++) {
             builder.append("," + strs[i]);
         }
         builder.append(" ]");
-        return builder.toString();
-    }
-
-    private String createResultNumArrStr(int[] numArr) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(numArr[0]);
-        int arrLen = numArr.length;
-        for (int i = 1; i < arrLen; i++) {
-            builder.append(" " + numArr[i]);
-        }
         return builder.toString();
     }
 }
