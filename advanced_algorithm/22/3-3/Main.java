b36cdb0e6065b1a74b2beb357730dcb490e561f8
zlic <161250197@smail.nju.edu.cn>
2021-03-15 17:29:32 +0800

    ✨feat(高级算法 22): 3-3 子矩阵问题。

--- advanced_algorithm/22/3-3/Main.java
+++ advanced_algorithm/22/3-3/Main.java
@@ -17,15 +17,57 @@ public class Main {
         String caseCountStr = this.scanner.nextLine();
         int caseCount = Integer.parseInt(caseCountStr);
         for (int i = 0; i < caseCount; i++) {
-            this.dealProblem();
+            this.findMax1Count();
+        }
+    }
+
+    class ArrToString<T> {
+        String exec(T[] arr) {
+            if (arr == null) {
+                return "null";
+            }
+            int arrLen = arr.length;
+            if (arrLen == 0) {
+                return "[]";
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.append("[ " + arr[0]);
+            for (int i = 1; i < arrLen; i++) {
+                builder.append("," + arr[i]);
+            }
+            builder.append(" ]");
+            return builder.toString();
+        }
+    }
+
+    class MatrixToString<T> {
+        String exec(T[][] matrix) {
+            if (matrix == null) {
+                return "null";
+            }
+            int arrLen = matrix.length;
+            if (arrLen == 0) {
+                return "[]";
+            }
+            ArrToString arrToString = new ArrToString<T>();
+            StringBuilder builder = new StringBuilder();
+            builder.append("[\n" + arrToString.exec(matrix[0]));
+            for (int i = 1; i < arrLen; i++) {
+                builder.append(",\n" + arrToString.exec(matrix[i]));
+            }
+            builder.append("\n]");
+            return builder.toString();
         }
     }
     
     public String toString() {
-        // TODO 修改属性信息
+        MatrixToString<Integer> integerMatrixToString = new MatrixToString<Integer>();
         StringBuilder builder = new StringBuilder();
         builder
-            .append("test: " + this.test + ";\n")
+            .append("row: " + this.row + ";\n")
+            .append("col: " + this.col + ";\n")
+            .append("matrix: " + integerMatrixToString.exec(this.matrix) + ";\n")
+            .append("max: " + this.max + ";\n")
         ;
         return builder.toString();
     }
@@ -34,26 +76,68 @@ public class Main {
         System.out.println(this);
     }
 
-    // TODO 添加属性
-    String test = "test";
+    int row = -1;
+    int col = -1;
+    Integer[][] matrix = null;
+    int max = -1;
 
-    // TODO 修改方法名称
-    private void dealProblem() {
+    private void findMax1Count() {
         loadData();
         calResult();
         printResult();
     }
 
     private void loadData() {
-        // TODO
+        String[] rowColStrArr = this.scanner.nextLine().split(" ");
+        this.row = Integer.parseInt(rowColStrArr[0]);
+        this.col = Integer.parseInt(rowColStrArr[1]);
+
+        this.matrix = new Integer[this.row][];
+        for (int r = 0; r < this.row; r++) {
+            String[] rowStrArr = this.scanner.nextLine().split(" ");
+            this.matrix[r] = new Integer[this.col];
+            for (int c = 0; c < this.col; c++) {
+                this.matrix[r][c] = Integer.parseInt(rowStrArr[c]);
+            }
+        }
+
+        this.max = 0;
     }
 
     private void calResult() {
-        // TODO
-        this.printData();
+        for (int rStart = 0; rStart < this.row; rStart++) {
+            for (int cStart = 0; cStart < this.col; cStart++) {
+                if (this.matrix[rStart][cStart] == 1) {
+                    this.calMax1Count(rStart, cStart);
+                }
+            }
+        }
+    }
+
+    private void calMax1Count(int rStart, int cStart) {
+        int maxCol = cStart + 1;
+        while (maxCol < this.col && this.matrix[rStart][maxCol] == 1) {
+            maxCol++;
+        }
+        this.updateMax(rStart, rStart + 1, cStart, maxCol);
+
+        for (int rEnd = rStart + 1; rEnd < this.row; rEnd++) {
+            int cEnd = cStart;
+            for (; cEnd < maxCol; cEnd++) {
+                if (this.matrix[rEnd][cEnd] == 0) {
+                    maxCol = cEnd;
+                    break;
+                }
+            }
+            this.updateMax(rStart, rEnd + 1, cStart, cEnd);
+        }
+    }
+
+    private void updateMax(int rStart, int rEnd, int cStart, int cEnd) {
+        this.max = Math.max(this.max, (rEnd - rStart) * (cEnd - cStart));
     }
 
     private void printResult() {
-        // TODO
+        System.out.println(this.max);
     }
 }
