c74871cf353ff5e801095971b9b95ae6ad8bbfc3
zlic <161250197@smail.nju.edu.cn>
2021-03-16 10:31:23 +0800

    ✨feat(高级算法 22): 3-4 能否成环

--- advanced_algorithm/22/3-4/Main.java
+++ advanced_algorithm/22/3-4/Main.java
@@ -17,15 +17,39 @@ public class Main {
         String caseCountStr = this.scanner.nextLine();
         int caseCount = Integer.parseInt(caseCountStr);
         for (int i = 0; i < caseCount; i++) {
-            this.dealProblem();
+            this.canBeRing();
+        }
+    }
+    
+    class ArrToString<T> {
+        String exec(T[] arr) {
+            if (arr == null) {
+                return "null";
+            }
+            int arrLen = arr.length;
+            if (arrLen == 0) {
+                return "[]";
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.append("[ " + arr[0]);
+            for (int i = 1; i < arrLen; i++) {
+                builder.append("," + arr[i]);
+            }
+            builder.append(" ]");
+            return builder.toString();
         }
     }
     
     public String toString() {
-        // TODO 修改属性信息
+        ArrToString<Character> characterArrToString = new ArrToString<>();
         StringBuilder builder = new StringBuilder();
         builder
-            .append("test: " + this.test + ";\n")
+            .append("strCount: " + this.strCount + ";\n")
+            .append("headChars: " + characterArrToString.exec(this.headChars) + ";\n")
+            .append("tailChars: " + characterArrToString.exec(this.tailChars) + ";\n")
+            .append("unusedArr: " + new ArrToString<Boolean>().exec(this.unusedArr) + ";\n")
+            .append("isRing: " + this.isRing + ";\n")
+            .append("usedCount: " + this.usedCount + ";\n")
         ;
         return builder.toString();
     }
@@ -34,26 +58,82 @@ public class Main {
         System.out.println(this);
     }
 
-    // TODO 添加属性
-    String test = "test";
+    int strCount = -1;
+    Character[] headChars = null;
+    Character[] tailChars = null;
+    Boolean[] unusedArr = null;
+    boolean isRing = false;
+    int usedCount = -1;
 
-    // TODO 修改方法名称
-    private void dealProblem() {
+    private void canBeRing() {
         loadData();
         calResult();
         printResult();
     }
 
     private void loadData() {
-        // TODO
+        this.strCount = Integer.parseInt(this.scanner.nextLine());
+        String[] strArr = this.scanner.nextLine().split(" ");
+        this.headChars = new Character[this.strCount];
+        this.tailChars = new Character[this.strCount];
+        this.unusedArr = new Boolean[this.strCount];
+        for (int i = 0; i < this.strCount; i++) {
+            String str = strArr[i];
+            int len = str.length();
+            this.headChars[i] = str.charAt(0);
+            this.tailChars[i] = str.charAt(len - 1);
+            this.unusedArr[i] = true;
+        }
+        this.isRing = false;
+        this.usedCount = 0;
     }
 
     private void calResult() {
-        // TODO
-        this.printData();
+        if (this.strCount == 1) {
+            this.isRing = this.headChars[0].equals(this.tailChars[0]);
+            return;
+        }
+        this.unusedArr[0] = false;
+        this.usedCount++;
+        this.findRing(this.headChars[0], this.tailChars[0]);
+    }
+
+    private void findRing(Character head, Character tail) {
+        for (int i = 1; i < this.strCount; i++) {
+            if (this.unusedArr[i]) {
+                if (this.headChars[i].equals(tail)) {
+                    this.unusedArr[i] = false;
+                    this.usedCount++;
+                    if (this.usedCount == this.strCount && head.equals(this.tailChars[i])) {
+                        this.isRing = true;
+                        return;
+                    }
+                    this.findRing(head, this.tailChars[i]);
+                    if (this.isRing) {
+                        return;
+                    }
+                    this.unusedArr[i] = true;
+                    this.usedCount--;
+                }
+                if (this.tailChars[i].equals(head)) {
+                    this.unusedArr[i] = false;
+                    this.usedCount++;
+                    if (this.usedCount == this.strCount && this.headChars[i].equals(tail)) {
+                        this.isRing = true;
+                        return;
+                    }
+                    this.findRing(this.headChars[i], tail);
+                    if (this.isRing) {
+                        return;
+                    }
+                    this.unusedArr[i] = true;
+                    this.usedCount--;
+                }
+            }
+        }
     }
 
     private void printResult() {
-        // TODO
+        System.out.println(this.isRing ? 1 : 0);
     }
 }
