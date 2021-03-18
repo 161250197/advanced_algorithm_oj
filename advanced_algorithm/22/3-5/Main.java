c8042bf94c0bfacacd4ec11fed34f11a16fc7038
zlic <161250197@smail.nju.edu.cn>
2021-03-18 22:08:18 +0800

    ✨feat(高级算法 22): 3-5 数字重组整除问题
    0 不可以被整除。

--- advanced_algorithm/22/3-5/Main.java
+++ advanced_algorithm/22/3-5/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Main {
     private Scanner scanner;
@@ -17,15 +19,36 @@ public class Main {
         String caseCountStr = this.scanner.nextLine();
         int caseCount = Integer.parseInt(caseCountStr);
         for (int i = 0; i < caseCount; i++) {
-            this.dealProblem();
+            this.findMax17Number();
+        }
+    }
+
+    class ArrToString<T> {
+        String exec(T[] arr) {
+            if (arr == null) {
+                return "null";
+            }
+            int arrLen = arr.length;
+            if (arrLen == 0) {
+                return "[]";
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.append("[ " + arr[0]);
+            for (int i = 1; i < arrLen; i++) {
+                builder.append("," + arr[i]);
+            }
+            builder.append(" ]");
+            return builder.toString();
         }
     }
     
     public String toString() {
-        // TODO 修改属性信息
         StringBuilder builder = new StringBuilder();
         builder
-            .append("test: " + this.test + ";\n")
+            .append("numCount: " + this.numCount + ";\n")
+            .append("numbers: " + new ArrToString<Integer>().exec(this.numbers) + ";\n")
+            .append("unusedArr: " + new ArrToString<Boolean>().exec(this.unusedArr) + ";\n")
+            .append("max17Number: " + this.max17Number + ";\n")
         ;
         return builder.toString();
     }
@@ -34,26 +57,70 @@ public class Main {
         System.out.println(this);
     }
 
-    // TODO 添加属性
-    String test = "test";
+    int numCount = -1;
+    Integer[] numbers = null;
+    Boolean[] unusedArr = null;
+    Integer max17Number = null;
 
-    // TODO 修改方法名称
-    private void dealProblem() {
+    private void findMax17Number() {
         loadData();
         calResult();
         printResult();
     }
 
     private void loadData() {
-        // TODO
+        this.max17Number = null;
+        String[] numberStrs = this.scanner.nextLine().split("");
+        this.numCount = numberStrs.length;
+        this.numbers = new Integer[this.numCount];
+        this.unusedArr = new Boolean[this.numCount];
+        for (int i = 0; i < this.numCount; i++) {
+            this.numbers[i] = Integer.parseInt(numberStrs[i]);
+            this.unusedArr[i] = true;
+        }
     }
 
     private void calResult() {
-        // TODO
-        this.printData();
+        this.createNewDivisible17(0, 0);
+    }
+
+    private void createNewDivisible17(int sum, int usedNumCount) {
+        Set<Integer> usedNumber = new HashSet<Integer>();
+        int newUsedNumCount = usedNumCount + 1;
+        for (int i = 0; i < this.numCount; i++) {
+            int num = this.numbers[i];
+            if (this.unusedArr[i] && !usedNumber.contains(num)) {
+                this.unusedArr[i] = false;
+                usedNumber.add(num);
+                int newSum = sum * 10 + num;
+                if (newUsedNumCount == this.numCount) {
+                    this.setMaxDivisible17(newSum);
+                } else {
+                    this.createNewDivisible17(newSum, newUsedNumCount);
+                }
+                this.unusedArr[i] = true;
+            }
+        }
+    }
+
+    private void setMaxDivisible17(int number) {
+        if (undivisble17(number)) {
+            return;
+        }
+        if (this.max17Number == null || this.max17Number < number) {
+            this.max17Number = number;
+        }
+    }
+
+    private boolean undivisble17(int number) {
+        return number % 17 != 0;
     }
 
     private void printResult() {
-        // TODO
+        if (this.max17Number == null || this.max17Number == 0) {
+            System.out.println("Not Possible");
+        } else {
+            System.out.println(this.max17Number);
+        }
     }
 }
