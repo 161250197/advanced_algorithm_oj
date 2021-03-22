96d685a8ab4e9bb44cfb1fb7644934fc2385019a
zlic <161250197@smail.nju.edu.cn>
2021-03-22 11:29:55 +0800

    ✨feat(高级算法 22): 3-7 和最大的连续降序字符。

--- advanced_algorithm/22/3-7/Main.java
+++ advanced_algorithm/22/3-7/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
     private Scanner scanner;
@@ -17,15 +19,34 @@ public class Main {
         String caseCountStr = this.scanner.nextLine();
         int caseCount = Integer.parseInt(caseCountStr);
         for (int i = 0; i < caseCount; i++) {
-            this.dealProblem();
+            this.calMaxSumDownQueue();
+        }
+    }
+    
+    class ArrToString<T> {
+        String exec(T[] arr) {
+            if (arr == null) {
+                return "null";
+            }
+            int arrLen = arr.length;
+            if (arrLen == 0) {
+                return "[]";
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.append("[ " + arr[0]);
+            for (int i = 1; i < arrLen; i++) {
+                builder.append("," + arr[i]);
+            }
+            builder.append(" ]");
+            return builder.toString();
         }
     }
     
     public String toString() {
-        // TODO 修改属性信息
         StringBuilder builder = new StringBuilder();
         builder
-            .append("test: " + this.test + ";\n")
+            .append("showed: " + new ArrToString<Boolean>().exec(this.showed) + ";\n")
+            .append("result: " + this.result + ";\n")
         ;
         return builder.toString();
     }
@@ -34,26 +55,72 @@ public class Main {
         System.out.println(this);
     }
 
-    // TODO 添加属性
-    String test = "test";
+    Boolean[] showed;
+    String result;
 
-    // TODO 修改方法名称
-    private void dealProblem() {
+    private void calMaxSumDownQueue() {
         loadData();
         calResult();
         printResult();
     }
 
     private void loadData() {
-        // TODO
+        char[] charArr = this.scanner.nextLine().toCharArray();
+        this.showed = new Boolean[26];
+        for (char c : charArr) {
+            this.showed['Z' - c] = true;
+        }
+        this.result = "";
     }
 
     private void calResult() {
-        // TODO
-        this.printData();
+        for (int diff = 1; diff < 26; diff++) {
+            this.findArithmeticQueue(diff);
+        }
+    }
+
+    private void findArithmeticQueue(int diff) {
+        Boolean[] usedArr = new Boolean[26];
+        for (int i = 0; i < 26; i++) {
+            if (this.showed[i] != null && usedArr[i] == null) {
+                usedArr[i] = true;
+                List<Integer> indexList = new ArrayList<>();
+                indexList.add(i);
+                int nextIndex = i + diff;
+                while (nextIndex < 26 && this.showed[nextIndex] != null && usedArr[nextIndex] == null) {
+                    usedArr[nextIndex] = true;
+                    indexList.add(nextIndex);
+                    nextIndex += diff;
+                }
+                String result = this.createResult(indexList);
+                this.setResult(result);
+            }
+        }
+    }
+
+    private String createResult(List<Integer> indexList) {
+        int len = indexList.size();
+        char[] charArr = new char[len];
+        int index = 0;
+        for (int i : indexList) {
+            charArr[index] = (char)('Z' - i);
+            index++;
+        }
+        return new String(charArr);
+    }
+
+    private void setResult(String newResult) {
+        int newLen = newResult.length();
+        int oldLen = this.result.length();
+        if (
+            newLen > oldLen || 
+            (newLen == oldLen && newResult.compareTo(this.result) > 0)
+        ) {
+            this.result = newResult;
+        }
     }
 
     private void printResult() {
-        // TODO
+        System.out.println(this.result);
     }
 }
